71e7cf3fbfdca32007af4299408239280b4ae147
iJuYoung <100898838+iJuYoung@users.noreply.github.com>
2024-07-17 15:20:42 +0900

    Update InclassSpringSecurityApplicationTests.java

--- src/test/java/ac/su/inclassspringsecurity/InclassSpringSecurityApplicationTests.java
+++ src/test/java/ac/su/inclassspringsecurity/InclassSpringSecurityApplicationTests.java
@@ -2,8 +2,12 @@ package ac.su.inclassspringsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class InclassSpringSecurityApplicationTests {
 
     @Test
