9c2c286815074e0dbf1831b63b35fe7b6e741965
iJuYoung <100898838+iJuYoung@users.noreply.github.com>
2024-07-17 15:10:37 +0900

    Update OrderProductServiceTest.java

--- src/test/java/ac/su/inclassspringsecurity/service/OrderProductServiceTest.java
+++ src/test/java/ac/su/inclassspringsecurity/service/OrderProductServiceTest.java
@@ -3,7 +3,7 @@ package ac.su.inclassspringsecurity.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.TestPropertySource;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
