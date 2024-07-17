45de92cb757905259aefd6c7a7fd89b3f3772c03
iJuYoung <100898838+iJuYoung@users.noreply.github.com>
2024-07-17 15:11:05 +0900

    Update OrderProductServiceTest.java

--- src/test/java/ac/su/inclassspringsecurity/service/OrderProductServiceTest.java
+++ src/test/java/ac/su/inclassspringsecurity/service/OrderProductServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.TestPropertySource;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class OrderProductServiceTest {
     @Autowired
     private OrderProductService orderProductService;
